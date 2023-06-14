d7b49a91174c4ad210ae6c8e020f0dd530c03dcf
Pegah Ymd s315908 <s315908@studenti.polito.it>
2023-06-14 14:43:50 +0200

    Adding image addresses in sqlite db and exported version

---
+++ client/src/assets/DB/images.sql
@@ -0,0 +1,8 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "images" (
+	"https://github.com/PegahYmd/Web-test/blob/adb5c47e8d5cfc5b60b344adf71994d3ad9fd8ee/images/1.jpg"	TEXT,
+	"https://github.com/PegahYmd/Web-test/blob/adb5c47e8d5cfc5b60b344adf71994d3ad9fd8ee/images/2.jpg"	TEXT,
+	"https://github.com/PegahYmd/Web-test/blob/adb5c47e8d5cfc5b60b344adf71994d3ad9fd8ee/images/3.jpg"	TEXT,
+	"https://github.com/PegahYmd/Web-test/blob/adb5c47e8d5cfc5b60b344adf71994d3ad9fd8ee/images/4.jpg"	TEXT
+);
+COMMIT;
