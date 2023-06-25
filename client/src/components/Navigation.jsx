08ced6d6009039055809ea406f44f4e9ca373db6
Pegah Ymd s315908 <s315908@studenti.polito.it>
2023-06-25 12:54:00 +0200

    Navigation settings

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { Navbar, Nav, Form } from 'react-bootstrap';
+import { Navbar, Nav, Form, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { LogoutButton, LoginButton } from './Auth';
 
@@ -12,32 +12,35 @@ const Navigation = (props) => {
   }
 
   return (
-    <Navbar bg="primary" expand="sm" variant="dark" fixed="top" className="navbar-padding justify-content-between">
+    <Navbar expand="sm" variant="dark" fixed="top" className="navbar-padding justify-content-between">
       <Link to="/">
         <Navbar.Brand>
-          {/* <i className="bi bi-collection-play icon-size"/> */}CMS
+          CMS
         </Navbar.Brand>
       </Link>
-      {/* <Form className="my-2 my-lg-0 mx-auto d-sm-block" action="#" role="search" aria-label="Quick search" onSubmit={handleSubmit}>
-        <Form.Control className="mr-sm-2" type="search" placeholder="Search" aria-label="Search query" />
-      </Form> */}
-      
-        <Nav.Item>
-          <Nav.Link href="#">
-            <Link to={"/admin"} state={{nextpage: location.pathname}}>
-              <p>admin pages</p>
-              </Link>
-            </Nav.Link>
 
-            <Link to={"/page/" + 1} state={{nextpage: location.pathname}}>
-            <p>regular user pages</p>
-            </Link>
-            
+        <Nav.Item>
+          <Container className='links-wid'>
+            <Row>
+              <Col lg={6}>
+                <Nav.Link href="#">
+                  <Link to={"/admin"} state={{nextpage: location.pathname}}>
+                    <p>admin pages</p>
+                  </Link>
+                </Nav.Link>
+             </Col>
+            <Col lg={6}>
+              <Link to={"/page/" + 1} state={{nextpage: location.pathname}}>
+                <p>regular user pages</p>
+              </Link>
+            </Col>
+            </Row>
+          </Container>
         </Nav.Item>
+        
         <Nav className="ml-md-auto">
         <Navbar.Text className="mx-2">
           {props.user && props.user.name && `Welcome, ${props.user.name}!`}
-          {/* <i className="bi bi-person-circle icon-size"/> */}
         </Navbar.Text>
         <Form className="mx-2">
           {props.loggedIn ? <LogoutButton logout={props.logout} /> : <LoginButton />}
